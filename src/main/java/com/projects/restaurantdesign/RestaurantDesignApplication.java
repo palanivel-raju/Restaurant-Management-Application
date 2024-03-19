dfbb54a8721c363b7114eeca793af60db0c71b22
Palanivel <palanivel.developer@gmail.com>
2024-03-19 13:44:56 +0530

    Initialize application

---
+++ src/main/java/com/projects/restaurantdesign/RestaurantDesignApplication.java
@@ -0,0 +1,13 @@
+package com.projects.restaurantdesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantDesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantDesignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projects/restaurantdesign/RestaurantDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.restaurantdesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantDesignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
